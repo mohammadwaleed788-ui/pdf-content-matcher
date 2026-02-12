a0a74f012ca2ad015783bb39261e90bd8bd4b2d6
mohammadwaleed788 <mohammadwaleed788@gmail.com>
2026-02-12 14:59:45 +0500

    output specified

--- backend/utils/generateExcel.js
+++ backend/utils/generateExcel.js
@@ -8,23 +8,23 @@ export const generateExcel = async (results, filePath) => {
     { header: "Bank Date", key: "bankDate", width: 15 },
     { header: "Bank Narration", key: "bankNarration", width: 30 },
     { header: "Bank Amount", key: "bankAmount", width: 15 },
-    { header: "Ledger Date", key: "ledgerDate", width: 15 },
-    { header: "Ledger Narration", key: "ledgerNarration", width: 30 },
-    { header: "Ledger Amount", key: "ledgerAmount", width: 15 },
+    // { header: "Ledger Date", key: "ledgerDate", width: 15 },
+    // { header: "Ledger Narration", key: "ledgerNarration", width: 30 },
+    // { header: "Ledger Amount", key: "ledgerAmount", width: 15 },
     { header: "Status", key: "status", width: 15 },
   ];
 
-  results.matches.forEach((m) => {
-    sheet.addRow({
-      bankDate: m.bank.date,
-      bankNarration: m.bank.narration,
-      bankAmount: m.bank.amount,
-      ledgerDate: m.ledger.date,
-      ledgerNarration: m.ledger.narration,
-      ledgerAmount: m.ledger.amount,
-      status: "MATCHED",
-    });
-  });
+  // results.matches.forEach((m) => {
+  //   sheet.addRow({
+  //     bankDate: m.bank.date,
+  //     bankNarration: m.bank.narration,
+  //     bankAmount: m.bank.amount,
+  //     ledgerDate: m.ledger.date,
+  //     ledgerNarration: m.ledger.narration,
+  //     ledgerAmount: m.ledger.amount,
+  //     status: "MATCHED",
+  //   });
+  // });
 
   results.unmatchedBank.forEach((b) => {
     sheet.addRow({
@@ -35,14 +35,14 @@ export const generateExcel = async (results, filePath) => {
     });
   });
 
-  results.unmatchedLedger.forEach((l) => {
-    sheet.addRow({
-      ledgerDate: l.date,
-      ledgerNarration: l.narration,
-      ledgerAmount: l.amount,
-      status: "ONLY IN LEDGER",
-    });
-  });
+  // results.unmatchedLedger.forEach((l) => {
+  //   sheet.addRow({
+  //     ledgerDate: l.date,
+  //     ledgerNarration: l.narration,
+  //     ledgerAmount: l.amount,
+  //     status: "ONLY IN LEDGER",
+  //   });
+  // });
 
   await workbook.xlsx.writeFile(filePath);
 };
