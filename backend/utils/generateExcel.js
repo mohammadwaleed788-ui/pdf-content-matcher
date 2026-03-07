7105bfe3d56dc854581ae2746c8b0fb2de1c3442
mohammadwaleed788 <mohammadwaleed788@gmail.com>
2026-03-07 06:18:59 +0500

    output formated

--- backend/utils/generateExcel.js
+++ backend/utils/generateExcel.js
@@ -9,26 +9,26 @@ export const generateExcel = async (results, filePath) => {
     { header: "Narration", key: "bankNarration", width: 30 },
     { header: "Debit", key: "bankDebit", width: 15 },
     { header: "Credit", key: "bankCredit", width: 15 },
-    { header: "Ledger Date", key: "ledgerDate", width: 15 },
-    { header: "Ledger Narration", key: "ledgerNarration", width: 30 },
-    { header: "Ledger Debit", key: "ledgerDebit", width: 15 },
-    { header: "Ledger Credit", key: "ledgerCredit", width: 15 },
+    // { header: "Ledger Date", key: "ledgerDate", width: 15 },
+    // { header: "Ledger Narration", key: "ledgerNarration", width: 30 },
+    // { header: "Ledger Debit", key: "ledgerDebit", width: 15 },
+    // { header: "Ledger Credit", key: "ledgerCredit", width: 15 },
     { header: "Status", key: "status", width: 15 },
   ];
 
-  results.matches.forEach((m) => {
-    sheet.addRow({
-      bankDate: m.bank.date,
-      bankNarration: m.bank.narration,
-      bankDebit: m.bank.debit || "",
-      bankCredit: m.bank.credit || "",
-      ledgerDate: m.ledger.date,
-      ledgerNarration: m.ledger.narration,
-      ledgerDebit: m.ledger.debit || "",
-      ledgerCredit: m.ledger.credit || "",
-      status: "MATCHED",
-    });
-  });
+  // results.matches.forEach((m) => {
+  //   sheet.addRow({
+  //     bankDate: m.bank.date,
+  //     bankNarration: m.bank.narration,
+  //     bankDebit: m.bank.debit || "",
+  //     bankCredit: m.bank.credit || "",
+  //     ledgerDate: m.ledger.date,
+  //     ledgerNarration: m.ledger.narration,
+  //     ledgerDebit: m.ledger.debit || "",
+  //     ledgerCredit: m.ledger.credit || "",
+  //     status: "MATCHED",
+  //   });
+  // });
 
   results.unmatchedBank.forEach((b) => {
     sheet.addRow({
@@ -40,15 +40,15 @@ export const generateExcel = async (results, filePath) => {
     });
   });
 
-  results.unmatchedLedger.forEach((l) => {
-    sheet.addRow({
-      ledgerDate: l.date,
-      ledgerNarration: l.narration,
-      ledgerDebit: l.debit || "",
-      ledgerCredit: l.credit || "",
-      status: "ONLY IN LEDGER",
-    });
-  });
+  // results.unmatchedLedger.forEach((l) => {
+  //   sheet.addRow({
+  //     ledgerDate: l.date,
+  //     ledgerNarration: l.narration,
+  //     ledgerDebit: l.debit || "",
+  //     ledgerCredit: l.credit || "",
+  //     status: "ONLY IN LEDGER",
+  //   });
+  // });
 
   await workbook.xlsx.writeFile(filePath);
 };
